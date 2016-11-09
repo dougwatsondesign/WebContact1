b441cde3955490220cfa0edeedf6fe66f09f55b5
Doug Watson <dougwatsondesign@gmail.com>
2016-11-09 16:33:00 -0500

    Initial Commit

---
+++ WebContact1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebContact1.MvcApplication" Language="C#" %>
---
+++ WebContact1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebContact1.Startup))]
+namespace WebContact1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
