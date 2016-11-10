8f5fae3c45956e054168802e1864d71ed6065732
Doug Watson <dougwatsondesign@gmail.com>
2016-11-10 13:02:56 -0500

    FB Fix

--- WebContact1/App_Start/Startup.Auth.cs
+++ WebContact1/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using WebContact1.Models;
+using System.Configuration;
 
 namespace WebContact1
 {
@@ -54,9 +55,9 @@ namespace WebContact1
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: ConfigurationManager.AppSettings["AppID"],
+               appSecret: ConfigurationManager.AppSettings["AppSecret"]);
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
---
+++ WebContact1/Models/ContactModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebContact1.Models
+{
+    public class ContactModel
+    {
+        public int ID { get; set; }
+        public Guid ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string AltPhone { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+
+    }
+}
