773219081588fdc34bba366f07a6121405c87b44
Doug Watson <dougwatsondesign@gmail.com>
2016-11-11 11:27:42 -0500

    Some changes

---
+++ WebContact1/Controllers/ContactModelsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebContact1.Models;
+
+namespace WebContact1.Controllers
+{
+    public class ContactModelsController : Controller
+    {
+        private WebContact1Context db = new WebContact1Context();
+
+        // GET: ContactModels
+        public ActionResult Index()
+        {
+            return View(db.ContactModels.ToList());
+        }
+
+        // GET: ContactModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactModel contactModel = db.ContactModels.Find(id);
+            if (contactModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactModel);
+        }
+
+        // GET: ContactModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContactModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,ContactID,FirstName,LastName,Email,Phone,AltPhone,Birthday,Address1,Address2,City,State,Zip")] ContactModel contactModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ContactModels.Add(contactModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactModel);
+        }
+
+        // GET: ContactModels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactModel contactModel = db.ContactModels.Find(id);
+            if (contactModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactModel);
+        }
+
+        // POST: ContactModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,ContactID,FirstName,LastName,Email,Phone,AltPhone,Birthday,Address1,Address2,City,State,Zip")] ContactModel contactModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contactModel);
+        }
+
+        // GET: ContactModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactModel contactModel = db.ContactModels.Find(id);
+            if (contactModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactModel);
+        }
+
+        // POST: ContactModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ContactModel contactModel = db.ContactModels.Find(id);
+            db.ContactModels.Remove(contactModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebContact1/Models/WebContact1Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebContact1.Models
+{
+    public class WebContact1Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public WebContact1Context() : base("name=WebContact1Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WebContact1.Models.ContactModel> ContactModels { get; set; }
+    }
+}
